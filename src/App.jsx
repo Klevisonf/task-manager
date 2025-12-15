67a6cb84789151132f0c664b289cf587abc762b4
Klevisonf <silvaklevison11@gmail.com>
2025-12-14 22:26:27 -0300

    chore: add git hooks with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
 
 function App() {
-
-
-
-
   return (
     <div>
       <h1>Home Page aaaa</h1>
